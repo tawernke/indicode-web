e2e8efa27383783be2ed86afd1b81cc7efef5f59
Tim <t.a.wernke@gmail.com>
2020-09-22 22:02:30 -0700

    Add login and navbar with different states

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-import { Provider, createClient } from "urql";
-
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { Provider, createClient, fetchExchange, dedupExchange } from "urql";
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from "../generated/graphql";
 import theme from "../theme";
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 function MyApp({ Component, pageProps }) {
   const client = createClient({
@@ -9,14 +19,54 @@ function MyApp({ Component, pageProps }) {
     fetchOptions: {
       credentials: "include",
     },
+    exchanges: [
+      dedupExchange,
+      cacheExchange({
+        updates: {
+          Mutation: {
+            login: (_result, args, cache, info) => {
+              betterUpdateQuery<LoginMutation, MeQuery>(
+                cache,
+                {
+                  query: MeDocument,
+                },
+                _result,
+                (result, query) => {
+                  if (result.login.errors) {
+                    return query;
+                  }
+
+                  return { me: result.login.user };
+                }
+              );
+            },
+            register: (_result, args, cache, info) => {
+              betterUpdateQuery<RegisterMutation, MeQuery>(
+                cache,
+                {
+                  query: MeDocument,
+                },
+                _result,
+                (result, query) => {
+                  if (result.register.errors) {
+                    return query;
+                  }
+
+                  return { me: result.register.user };
+                }
+              );
+            },
+          },
+        },
+      }),
+      fetchExchange,
+    ],
   });
   return (
     <Provider value={client}>
       <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
+        <CSSReset />
+        <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
   );

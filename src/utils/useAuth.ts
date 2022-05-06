d112f3b69f9a6f79113e7425efdf38a5e900b6fa
Tim Wernke <t.a.wernke@gmail.com>
2022-05-06 07:02:38 -0700

    Changed: use /login page in auth hook

--- src/utils/useAuth.ts
+++ src/utils/useAuth.ts
@@ -1,13 +1,13 @@
-import { useMeQuery } from "../generated/graphql";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useMeQuery } from '../generated/graphql';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export const useAdminAuth = () => {
   const { data, loading } = useMeQuery();
   const router = useRouter();
   useEffect(() => {
     if (!loading && !data?.me) {
-      router.replace(`/admin/login?next=${router.pathname}`);
+      router.replace(`/login?next=${router.pathname}`);
     }
   }, [loading, data, router]);
   return { data, loading };

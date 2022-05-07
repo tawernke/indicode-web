baccf0178a2fed7276c2296aa6fe2ed1efb42fa3
Tim Wernke <t.a.wernke@gmail.com>
2022-05-07 09:42:00 -0700

    Added: test that admin user can see admin route

--- src/__tests__/Admin.test.tsx
+++ src/__tests__/Admin.test.tsx
@@ -7,13 +7,13 @@
 import React from 'react';
 import { act, render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
-import { MeDocument } from '../generated/graphql';
+import { MeDocument, ProductsDocument } from '../generated/graphql';
 import AllProducts from '../pages/admin';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import { createMockRouter } from '../test-utils/createMockRouter';
 import LayoutSwitch from '../components/LayoutSwitch';
  
-const mocks = [
+const noUserMock = [
   {
     request: {
       query: MeDocument,
@@ -25,13 +25,59 @@ const mocks = [
     },
   },
 ];
+
+const userMock = [
+  {
+    request: {
+      query: MeDocument,
+    },
+    result: {
+      data: {
+        me: {
+          id: 1,
+          username: 'username',
+          password: 'passoword',
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: ProductsDocument,
+      variables: {
+        limit: 15,
+      },
+    },
+    result: {
+      data: {
+        publicProducts: {
+          hasMore: false,
+          publicProducts: [{
+            createdAt: '1651067825812',
+            deleted: false,
+            id: 1,
+            imageUrl: 'https://res.cloudinary.com/dsekfgaelifhl.jpg',
+            isPublic: true,
+            isSold: false,
+            name: 'Test product',
+            price: 100,
+            quantity: 1,
+            updatedAt: '1651067825812',
+            uuid: '4568985463-e153-4642-b819-3856298',
+            __typename: 'Product',
+          }],
+        },
+      },
+    },
+  },
+];
  
 test('admin page does not display when user is not logged in', async () => {
   const pathname = '/admin';
   const router = createMockRouter({ pathname });
 
   render(
-    <MockedProvider mocks={mocks}>
+    <MockedProvider mocks={noUserMock}>
       <RouterContext.Provider value={router}>
         <LayoutSwitch>
           <AllProducts />
@@ -47,5 +93,20 @@ test('admin page does not display when user is not logged in', async () => {
   expect(router.replace).toHaveBeenCalledWith(
     `/login?next=${pathname}`,
   );
+
 });
- 
+
+test('admin page shows when user is logged in', async () => {
+  const pathname = '/admin';
+  const router = createMockRouter({ pathname });
+
+  render(
+    <MockedProvider mocks={userMock}>
+      <RouterContext.Provider value={router}>
+        <LayoutSwitch>
+          <AllProducts />
+        </LayoutSwitch>
+      </RouterContext.Provider>
+    </MockedProvider>,
+  );
+});
